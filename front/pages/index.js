6c9d354e637543305fd267c0ad1ce103afd37c45
unknown <00ljw00@naver.com>
2021-02-05 23:34:33 +0900

    노드버드 초기작업

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import AppLayout from '../components/AppLayout';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>난죠 요시노</div>
+    </AppLayout>
+  )
+}
+
+export default Home;
